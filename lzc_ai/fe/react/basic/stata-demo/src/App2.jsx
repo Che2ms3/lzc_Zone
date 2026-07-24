25d0b9a524395d04fa16ccaffb3a4c8bee1eb1a0
1391247022-code <1391247022@qq.com>
2026-07-24 14:40:26 +0800

    feat: add benchmark concept readme and stata-demo React project

---
+++ lzc_ai/fe/react/basic/stata-demo/src/App2.jsx
@@ -0,0 +1,26 @@
+import {
+  useState
+} from 'react';
+
+function App(){
+  const [count,setCount] = useState(0);
+  const addCount = () =>{
+    user.name.includes(filterText)
+    // setCount(count + 1); // 修改状态 异步
+    // console.log(count);// 同步 0 
+    // setCount(count + 1);
+    // setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+  }
+
+  return (
+    <>
+      <p>当前计数：{count} </p>
+      <button onClick={addCount}>+3</button>
+    </>
+  )
+}
+
+export default App
